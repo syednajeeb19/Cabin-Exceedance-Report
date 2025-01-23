33d7f29e1144f0d66859252fcf9b18da192c3aab
syednajeeb19 <syednajeeb19@gmail.com>
2025-01-23 09:40:24 +0530

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -10,25 +10,25 @@ import numpy as np
 
 def data_cleaning(src_path):
     df=pd.read_csv(src_path)
-    df.drop(columns = ['EQUIPMENT_CODE', 'TIME_PATTERN', 'REPORTING_TIME', 'FT_EXCEEDENCE', 'FDP_EXCEEDENCE', 'FT_EXCEEDENCE_TOTAL', 'FDP_EXCEEDENCE_TOTAL'], inplace = True)
-    df['LOCATION_PATTERN'] = df['LOCATION_PATTERN'].str.rstrip(' , ')
-    df['DUTY_PATTERN'] = 'AI' + df['DUTY_PATTERN'].str.replace('/', ' AI')
-    df['ACTUAL_PATTERN'] = df['LOCATION_PATTERN'] + '--' + df['DUTY_PATTERN']
-    df['UNIQUE_CONCATENATE'] = df['START_DATE'] + df['ACTUAL_PATTERN']
-    df['STAFF_NUMBER'] = df['STAFF_NUMBER'].astype('str')
-    df['NAME WITH SAP_ID'] = df['NAME'] + ' (' + df['STAFF_NUMBER'] + ')'
-    df.drop(columns = ['NAME', 'STAFF_NUMBER'], inplace = True)
-    df['TOTAL_CREW_MEMBERS'] = df.groupby('UNIQUE_CONCATENATE')['NAME WITH SAP_ID'].transform('nunique')
-    df.sort_values(by = 'UNIQUE_CONCATENATE', inplace = True)
+    df.drop(columns = ['Reporting Time', 'Time Pattern', 'FT Exceedance\n(Single Event) \n(in HH:mm)\n        ','FDP Exceedance\n(Single Event)\n(in HH:mm)\n        '], inplace = True)
+    df['Location Pattern'] = df['Location Pattern'].str.rstrip(' , ')
+    df['Flight Number(s)'] = df['Flight Number(s)'].str.replace('/', ' ')
+    df['Actual_Pattern'] = df['Location Pattern'] + '--' + df['Flight Number(s)']
+    df['Unique_Concatenate'] = df['Flight Date'] + df['Actual_Pattern']
+    df['Staff Number'] = df['Staff Number'].astype('str')
+    df['Name With SAP_ID'] = df['Crew Name'] + ' (' + df['Staff Number'] + ')'
+    df.drop(columns = ['Crew Name', 'Staff Number'], inplace = True)
+    df['Total_Crew_Members'] = df.groupby('Unique_Concatenate')['Name With SAP_ID'].transform('nunique')
+    df.sort_values(by = 'Unique_Concatenate', inplace = True)
     df.reset_index(drop = True, inplace = True)
-    df['START_DATE']=pd.to_datetime(df['START_DATE'],format='mixed')
     
     clean_data=df.copy()
-    
-    df_grouped = df.groupby('UNIQUE_CONCATENATE')['NAME WITH SAP_ID'].apply(lambda x : ', '.join(x)).reset_index()
-    df_merged = df.drop('NAME WITH SAP_ID', axis = 1).merge(df_grouped, on = 'UNIQUE_CONCATENATE')
-    df_merged.drop_duplicates(subset = 'UNIQUE_CONCATENATE', inplace = True)
-    
+
+    df_grouped = df.groupby('Unique_Concatenate')['Name With SAP_ID'].apply(lambda x : ', '.join(x)).reset_index()
+    df_merged = df.drop('Name With SAP_ID', axis = 1).merge(df_grouped, on = 'Unique_Concatenate')
+    df_merged.drop_duplicates(subset = 'Unique_Concatenate', inplace = True)
+    df_merged.drop(columns = ['Rank', 'Base', 'Actual_Pattern', 'Unique_Concatenate'], inplace = True)
+        
     
     return clean_data,df_merged
 
